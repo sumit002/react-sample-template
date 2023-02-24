a76bb845b78cd9979c8ce6376191a106508c4b48
Sumit Kumar Das <summit.dash01@gmail.com>
2023-02-25 02:15:40 +0530

    Creating Index.js

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM, { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
+// scroll bar
+import 'simplebar/src/simplebar.css';
+
+// third-party
+import { Provider as ReduxProvider } from 'react-redux';
+
+// project import
+import App from './App';
+import { store } from 'store';
+import reportWebVitals from './reportWebVitals';
+
+// ==============================|| MAIN - REACT DOM RENDER  ||============================== //
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(
+  <React.StrictMode>
+    <ReduxProvider store={store}>
+        <BrowserRouter basename="/">
+            <App />
+        </BrowserRouter>
+    </ReduxProvider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
